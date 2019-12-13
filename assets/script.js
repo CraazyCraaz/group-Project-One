4252ddf09ca9fa987868e27ebe17dd30912ed1d9
Phillip Dunham <phillipwwd@gmail.com>
2019-12-13 12:38:14 -0800

    Deck can be logged and shuffled test button click handler function in place

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,30 @@
  * CARD API
  ***************************************************************/
 
+var mainDeck = "";
+var newDeckUrl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+function newDeck(){
+    $.get(newDeckUrl).then(function(response){
+        console.log(response);
+        mainDeck = response.deck_id;  
+        console.log(mainDeck);
+    })
+};
 
 
 
 
- /***************************************************************
- * DRINK API
- ***************************************************************/
+
+
+
+
+$("button").on("click", function(){ //Generic test button handler. This makes all the buttons call whatever funcion is within. You can comment out the function and put in your own for test purposes.
+    newDeck(); // Calls for a new deck. Replaces deck ID.
+})
+
+
+
+
+//  /***************************************************************
+//  * DRINK API
+//  ****************************************************************
