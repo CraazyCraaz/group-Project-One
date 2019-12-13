fc6625aa7a14369d1ece3be1ef160b8522c37f7d
Phillip Dunham <phillipwwd@gmail.com>
2019-12-13 15:26:52 -0800

    labelled buttons work, initial deal works

--- assets/script.js
+++ assets/script.js
@@ -2,39 +2,44 @@
  * CARD API
  ***************************************************************/
 
-var mainDeck = "4ngaeo0qkd4m"; //for testing purposes
-var newDeckUrl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
 function newDeck(){
+    var newDeckUrl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
     $.get(newDeckUrl).then(function(response){
         console.log(response);
-        mainDeck = response.deck_id;  
+        var mainDeck = response.deck_id;
         console.log(mainDeck);
-    })
+        var shuffleUrl = "https://deckofcardsapi.com/api/deck/" + mainDeck + "/shuffle/"
+        
+        var p1stack = "";
+        var p2stack = "";
+        var dealHalfUrlp1 = "https://deckofcardsapi.com/api/deck/" + mainDeck + "/draw/?count=26"
+        
+        function firstDeal(){
+            $.get(dealHalfUrlp1).then(function(response){
+                console.log(response);
+            })
+        }
+        
+        function shuffle(){
+            $.get(shuffleUrl).then(function(response){
+                console.log(response);
+            });
+        }
+        
+        $("#deal").on("click", function(){ 
+            firstDeal(); 
+        });
+        $("#shuffle").on("click", function(){
+            shuffle();
+        });
+    });
 };
+                                        $("#newDeck").on("click", function(){
+                                            newDeck();
+                                        });
 
-var p1stack = "";
-var p2stack = "";
-var dealHalfUrlp1 = "https://deckofcardsapi.com/api/deck/" + mainDeck + "/draw/?count=26"
-function firstDeal(){
-    $.get(dealHalfUrlp1).then(function(response){
-        console.log(response);
-    })
-}
-
-
-
-
-
-
-
-$("a").on("click", function(){ //Generic test button handler. This makes all the buttons call whatever funcion is within. You can comment out the function and put in your own for test purposes.
-    //newDeck(); // Calls for a new deck. Replaces deck ID.
-    firstDeal();
-})
-
-
-
-
-//  /***************************************************************
+        
+        
+        //  /***************************************************************
 //  * DRINK API
 //  ****************************************************************
