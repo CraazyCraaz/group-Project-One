25a87652fc2b125e1bfccb6ef2beb9ed5973c066
Jay Little <jtlittle30@gmail.com>
2019-12-16 10:19:56 -0800

    added instructions button and changed navbar color and look and feel

--- assets/script.js
+++ assets/script.js
@@ -130,3 +130,15 @@ $("#randomDrink").on("click", function (event) {
 
     randomDrinkInfo();
 });
+
+// MODAL EVENT LISTENER
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems, options);
+//   });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
