47fde59c02565c5ee6fd7cbb20cdf45e5b73a9c0
Jay Little <jtlittle30@gmail.com>
2019-12-16 11:34:34 -0800

    animate testing

--- assets/script.js
+++ assets/script.js
@@ -150,13 +150,6 @@ $("#randomDrink").on("click", function (event) {
 });
 
 // MODAL EVENT LISTENER
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.modal');
-//     var instances = M.Modal.init(elems, options);
-//   });
-
-  // Or with jQuery
-
   $(document).ready(function(){
     $('.modal').modal();
   });
